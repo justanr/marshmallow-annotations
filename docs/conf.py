0b5348591abedf6fb95c24539aa168a93104a248
Alec Nikolas Reiter <alecreiter@gmail.com>
2018-08-05 19:30:33 -0400

    Bump version: 2.2.0 → 2.3.0

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = "2018, Alec Nikolas Reiter"
 author = "Alec Nikolas Reiter"
 
 # The short X.Y version
-version = "2.2.0"
+version = "2.3.0"
 # The full version, including alpha/beta/rc tags
-release = "2.2.0"
+release = "2.3.0"
 
 
 # -- General configuration ---------------------------------------------------
